f90be60580af603755911290e4e056e89d2383d5
TrueDanuch <73480196+TrueDanuch@users.noreply.github.com>
2021-04-08 21:28:58 +0300

    Add files via upload

--- sprint09/t00/db.sql
+++ sprint09/t00/db.sql
@@ -1,7 +1,6 @@
-CREATE DATABASE sword;
 USE sword;
 CREATE TABLE IF NOT EXISTS users(
-    login VARCHAR(50) NOT NULL,
+    login VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(32) NOT NULL,
     full_name TEXT NOT NULL,
     email_address TEXT NOT NULL
--- sprint09/t00/index.php
+++ sprint09/t00/index.php
@@ -1,3 +1,22 @@
+<?php
+$link = mysqli_connect('127.0.0.1', 'dyurchenko', 'securepass', 'sword');
+
+    if($_POST){
+        $login = $_POST["login"];
+        $password = $_POST["password"];
+        $fullName = $_POST["fullName"];
+        $emailAddress = $_POST["emailAddress"];
+
+        $sql = "INSERT INTO users (login, password, full_name, email_address) VALUES ('$login', '$password', '$fullName', '$emailAddress');";
+        
+        $result = mysqli_query($link, $sql);
+
+        if ($result == false) {
+            echo "Я даун";
+        }
+    }
+
+?> 
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +24,7 @@
     <meta charset="utf-8">
     <title>Registration</title>
     <meta name="description" content="t00. Registration">
-    <link href="style.css" rel="stylesheet">
+    <link type="text/css" href="style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -14,23 +33,23 @@
         <form method="post" action="index.php">
             <div class="input-group">
                 <label>Login:</label>
-                <input type="text" name="login" value="<?php echo $login; ?>">
+                <input type="text" name="login" id="login">
             </div>
             <div class="input-group">
                 <label>Password:</label>
-                <input type="password" name="password" value="<?php echo $password; ?>">
+                <input type="password" name="password">
             </div>
             <div class="input-group">
                 <label>Confirm Password:</label>
-                <input type="password" name="cPassword" value="<?php echo $cPassword; ?>">
+                <input type="password" name="cPassword">
             </div>
             <div class="input-group">
                 <label>Full Name:</label>
-                <input type="text" name="fullName" value="<?php echo $fullName; ?>">
+                <input type="text" name="fullName">
             </div>
             <div class="input-group">
                 <label>Email Address:</label>
-                <input type="email" name="emailAddress" value="<?php echo $emailAddress; ?>">
+                <input type="email" name="emailAddress">
             </div>
             <div class="input-group">
                 <button type="submit" class="btn" name="reg_user">Register</button>
