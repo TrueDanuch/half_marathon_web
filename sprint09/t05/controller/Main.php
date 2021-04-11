65228afe6d19d06a9325eb6352e00c9d6cb7b57a
TrueDanuch <73480196+TrueDanuch@users.noreply.github.com>
2021-04-11 18:36:24 +0300

    Add files via upload

---
+++ sprint09/t05/controller/Main.php
@@ -0,0 +1,22 @@
+<?php
+include __DIR__.'/../view/View.php';
+
+interface ControllerInterface{
+    public function __construct();
+    public function execute();
+}
+
+class Main implements ControllerInterface{
+    private $view;
+
+    public function __construct(){
+        $this->view = new View(__DIR__.'/../view/templates/main.html');
+    }
+
+    public function execute(){
+        if ($this->view){
+            $this->view->render();
+        }
+    }
+}
+?>
