26950fbcf2f26fad8a6cf2a875be023611e72a5b
TrueDanuch <73480196+TrueDanuch@users.noreply.github.com>
2021-04-14 22:05:45 +0300

    Add files via upload

---
+++ race01/server/DbConnection.js
@@ -0,0 +1,111 @@
+module.exports = class DbConnection {
+  #connection;
+  mysql;
+  constructor() {
+    this.mysql = require('mysql2')
+
+    this.connectToDatabase()
+    this.#connection.connect((err) => {
+      /* on error */
+      if (err) console.error(`Connection to MySQL failed: ${err.message}`)
+      else console.log(`Connected to MySQL`)
+    })
+
+    this.#connection.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        login VARCHAR(255) NOT NULL,
+        password_hash VARCHAR(255) NOT NULL,
+        avatar_link VARCHAR(255) DEFAULT 'https://ibb.co/s1WVzKT',
+        level INT(10) NOT NULL DEFAULT 0
+      );`, (err, results, fields) => { 
+        if (err) console.log(`Database creation error: ${err}\n`) 
+      })
+  }
+
+  connectToDatabase(user = "dyurchenko", password = "securepass", host = "localhost", port = 3306) {
+    this.#connection = this.mysql.createConnection({
+      host: host,
+      user: user,
+      database: "race01_db",
+      password: password,
+      port: port
+    })
+  }
+
+  showUsers() {
+    this.#connection.query("SELECT * FROM `users`", (err, results, fields) => {
+      if (err) console.log(`getUsers error: ${err}\n`)
+      else console.log(results)
+    })
+  }
+
+  requestUser(login, onResult) {
+    this.#connection.query(`SELECT * FROM users WHERE login = '${login}'`,
+    (err, results, fields) => {
+      if (err) {
+        console.log(`User request error: ${err}\n`)
+        onResult(null)
+      } else {
+        if (results.length >= 1) {
+          onResult({login: results[0].login, password_hash: results[0].password_hash})
+        }
+      }
+    })
+  }
+
+  registerUser(login, password_hash) {
+    this.#connection.query(
+      `INSERT INTO users (login, password_hash) 
+      VALUES ('${login}', '${password_hash}')`)
+  }
+
+  tryRegister(login, password_hash, onResult) {
+    this.#connection.query(
+      `SELECT * FROM users WHERE login = '${login}'`,
+      (err, results, fields) => {
+        if (err) {
+          console.log(`Registration error: ${err}\n`)
+          onResult(false)
+        } else {
+          if (results.length > 0) {
+            console.log("Registration failed!")
+            onResult(false)
+          } else {
+            // REGISTRATION HERE (no users with the same login found)
+            console.log(`Registered: ${login} ${password_hash}`)
+            this.registerUser(login, password_hash)
+            onResult(true)
+          }
+        }
+      }
+    )
+  }
+
+  tryLogin(login, password_hash, onResult) {
+    this.#connection.query(
+      `SELECT * FROM users WHERE login = '${login}'`,
+      (err, results, fields) => {
+        if (err) {
+          console.log(`Login error: ${err}\n`)
+          onResult(false)
+        }
+        else {
+          if (results.length > 0 && results[0].password_hash == password_hash) {
+            console.log("Login approved!")
+            onResult(true)
+          } else {
+            console.log("Login failed!")
+            onResult(false)
+          }
+        }
+      }
+    )
+  }
+
+  checkLogin(login, onResult) {
+    this.#connection.query(
+      `SELECT * FROM users WHERE login = '${login}'`,
+      (err, results, fields) => { onResult(results.length > 0) })
+  }
+}
---
+++ race01/server/server.js
@@ -0,0 +1,69 @@
+const port = 7070
+const host = '10.11.4.6'
+exports.host = host
+
+const http = require('http').createServer()
+const io = require('socket.io')(http, {
+  cors: { origin: "*" }
+});
+
+const DbConnection = require('./DbConnection.js')
+const User = require('./User.js')
+
+const db_connection = new DbConnection()
+// db_connection.getUsers()
+
+let Users = new Map()
+io.on('connection', (socket) => {
+  console.log(`User (${socket.handshake.address.split('f:')[1]}) connected.`)
+  Users.set(socket.handshake.address, new User(socket))
+
+  socket.on('message', text => {
+    console.log(text)
+  })
+  socket.on('try_login', (data) => {
+    console.log(data)
+    db_connection.tryLogin(data.login, data.password_hash, result => {
+      if (result == true) {
+        db_connection.requestUser(data.login, (login_password) => {
+          if (login_password) {
+            let user = Users.get(socket.handshake.address)
+            // console.log("FUUUCK " + user.socket.handshake.address.split('f:')[1])
+            user.log_in(login_password.login, login_password.password_hash)
+          } else {
+            console.log("User request failed!")
+          }
+        })
+      }
+      socket.emit('login_result', result)
+    })
+  })
+  socket.on('logout', () => {
+    Users.get(socket.handshake.address).logout()
+  })
+  socket.on('check_login', (data) => {
+    db_connection.checkLogin(data.login, result => {
+      socket.emit('check_login_result', result)
+    })
+  })
+  socket.on('try_register', (data) => {
+    db_connection.tryRegister(data.login, data.password_hash, result => {
+      socket.emit('register_result', result)
+    })
+  })
+  socket.on('disconnect', () => {
+    console.log(`User (${socket.handshake.address.split('f:')[1]}) disconnected.`)
+    Users.delete(socket.handshake.address)
+  })
+});
+
+http.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
+
+setInterval(() => {
+  console.log("=========================")
+  Users.forEach((user) => {
+    console.log(user.serialize())
+  })
+}, 2000)
